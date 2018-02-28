2687ae0d427efb06a409a081344dd7a920c9c166
Thomas Pönitz <thomas.poenitz@profactor.at>
2018-02-28 15:11:44 +0100

    cleaned up the testpackage

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -1,23 +1,16 @@
 from conans import ConanFile, CMake
-import os
-import sys
-
-
-channel = os.getenv("CONAN_CHANNEL", "stable")
-username = os.getenv("CONAN_USERNAME", "memsharded")
-
 
 class Pybind11TestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    requires = "pybind11/any@%s/%s" % (username, channel)
     generators = "cmake"
 
     def build(self):
-        cmake = CMake(self.settings)
-        self.run('cmake "%s" %s' % (self.conanfile_directory, cmake.command_line))
-        self.run("cmake --build . %s" % cmake.build_config)
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
 
     def test(self):
-        sys.path.append(".")
+        import sys
+        sys.path.append("bin")
         import example
-        self.output.info("Add %s" % example.add(2, 3))
+        assert example.add(2, 40) == 42
